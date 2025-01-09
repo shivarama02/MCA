d5e66e861304816f17def8f970c960a173f35111
Deepak H <deepakdeepam003@gmail.com>
2025-01-09 21:54:09 +0530

    pages updated

--- assets/script.js
+++ assets/script.js
@@ -23,136 +23,6 @@ fetch("https://api.github.com/repos/CET-MCA-26/MCA-Laboratory/contributors")
     console.error("Error fetching contributors:", error);
   });
 
-// // leaderboard
-// const leaderboardTable = document
-//   .getElementById("leaderboard")
-//   .querySelector("tbody");
-
-// // Fetch contributors from the GitHub API
-// fetch("https://api.github.com/repos/CET-MCA-26/MCA-Laboratory/contributors")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     // Sort contributors by contributions (highest first)
-//     data.sort((a, b) => b.contributions - a.contributions);
-
-//     // Clear the table body
-//     leaderboardTable.innerHTML = "";
-
-//     // Populate the leaderboard
-//     let rank = 1; // Separate counter for rank
-//     data.forEach((contributor) => {
-//       if (contributor.login === "jefintp") {
-//         return; // Skip the iteration for "jefintp"
-//       }
-//       else if (contributor.login === "yadhukrishnx") {
-//         return; // Skip the iteration for "jefintp"
-//       }
-//       const row = document.createElement("tr");
-//       row.innerHTML = `
-//         <td>${rank}</td> <!-- Use the separate rank counter -->
-//         <td>
-//             <img src="${contributor.avatar_url}" alt="${contributor.login}" class="contributor-img">
-//             <a href="${contributor.html_url}" target="_blank">${contributor.login}</a>
-//         </td>
-//         <td>${contributor.contributions}</td>
-//     `;
-//       leaderboardTable.appendChild(row);
-//       rank++; // Increment the rank counter only when adding a contributor
-//     });
-//   })
-//   .catch((error) => {
-//     leaderboardTable.innerHTML = `
-//             <tr>
-//                 <td colspan="3" style="text-align: center; color: red;">Failed to load data. Please try again later.</td>
-//             </tr>
-//         `;
-//     console.error("Error fetching contributors:", error);
-//   });
-
-document.addEventListener("DOMContentLoaded", function () {
-  const snowContainer = document.querySelector(".snow-container");
-
-  const particlesPerThousandPixels = 0.1;
-  const fallSpeed = 1.25;
-  const pauseWhenNotActive = true;
-  const maxSnowflakes = 200;
-  const snowflakes = [];
-
-  let snowflakeInterval;
-  let isTabActive = true;
-
-  function resetSnowflake(snowflake) {
-    const size = Math.random() * 5 + 1;
-    const viewportWidth = window.innerWidth - size; // Adjust for snowflake size
-    const viewportHeight = window.innerHeight;
-
-    snowflake.style.width = `${size}px`;
-    snowflake.style.height = `${size}px`;
-    snowflake.style.left = `${Math.random() * viewportWidth}px`; // Constrain within viewport width
-    snowflake.style.top = `-${size}px`;
-
-    const animationDuration = (Math.random() * 3 + 2) / fallSpeed;
-    snowflake.style.animationDuration = `${animationDuration}s`;
-    snowflake.style.animationTimingFunction = "linear";
-    snowflake.style.animationName =
-      Math.random() < 0.5 ? "fall" : "diagonal-fall";
-
-    setTimeout(() => {
-      if (parseInt(snowflake.style.top, 10) < viewportHeight) {
-        resetSnowflake(snowflake);
-      } else {
-        snowflake.remove(); // Remove when it goes off the bottom edge
-      }
-    }, animationDuration * 1000);
-  }
-
-  function createSnowflake() {
-    if (snowflakes.length < maxSnowflakes) {
-      const snowflake = document.createElement("div");
-      snowflake.classList.add("snowflake");
-      snowflakes.push(snowflake);
-      snowContainer.appendChild(snowflake);
-      resetSnowflake(snowflake);
-    }
-  }
-
-  function generateSnowflakes() {
-    const numberOfParticles =
-      Math.ceil((window.innerWidth * window.innerHeight) / 1000) *
-      particlesPerThousandPixels;
-    const interval = 5000 / numberOfParticles;
-
-    clearInterval(snowflakeInterval);
-    snowflakeInterval = setInterval(() => {
-      if (isTabActive && snowflakes.length < maxSnowflakes) {
-        requestAnimationFrame(createSnowflake);
-      }
-    }, interval);
-  }
-
-  function handleVisibilityChange() {
-    if (!pauseWhenNotActive) return;
-
-    isTabActive = !document.hidden;
-    if (isTabActive) {
-      generateSnowflakes();
-    } else {
-      clearInterval(snowflakeInterval);
-    }
-  }
-
-  generateSnowflakes();
-
-  window.addEventListener("resize", () => {
-    clearInterval(snowflakeInterval);
-    setTimeout(generateSnowflakes, 1000);
-  });
-
-  document.addEventListener("visibilitychange", handleVisibilityChange);
-});
-
-
-
 // Edit 
 
 function openModal(content) {
@@ -200,24 +70,24 @@ function openModal(content) {
           <h3>ADVANCED DATA STRUCTURE LAB</h3>
           <h4>Lab Cycle</h4>
           <ul>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/blob/main/S1/DS/array.c">Array</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/blob/main/S1/DS/stack.c">Stack</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/blob/main/S1/DS/queue.c">Queue</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/blob/main/S1/DS/Circular(singly).c">Circular Queue</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/blob/main/S1/DS/linkedlist.c">Linked List</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/blob/main/S1/DS/Doubly_Linked_List.c">Doubly Linked List</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/blob/main/S1/DS/Circular_DLL.c">Circular Linked List</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/blob/main/S1/DS/Bitstringg.c">Set using Bit String</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/DS/array.c">Array</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/DS/stack.c">Stack</a></li>
+            <li><a href="https://github.com/cetmca266/MCA-Laboratory/blob/main/S1/DS/queue.c">Queue</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/DS/Circular(singly).c">Circular Queue</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/DS/linkedlist.c">Linked List</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/DS/Doubly_Linked_List.c">Doubly Linked List</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/DS/Circular_DLL.c">Circular Linked List</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/DS/Bitstringg.c">Set using Bit String</a></li>
           </ul>
           <h4>Lab Cycle 2</h4>
           <ul>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/blob/main/S1/DS/BST.c">Binary Search Tree</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/blob/main/S1/DS/prims.c">Prim's Algorithm</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/blob/main/S1/DS/kruskals.c">Kruskal's Algorithm</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/blob/main/S1/DS/djikstra.c">Dijkstra's Algorithm</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/blob/main/S1/DS/topological_sort.c">Topological Sorting</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/blob/main/S1/DS/dfs.c">DFS</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/blob/main/S1/DS/bfs.c">BFS</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/DS/BST.c">Binary Search Tree</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/DS/prims.c">Prim's Algorithm</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/DS/kruskals.c">Kruskal's Algorithm</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/DS/djikstra.c">Dijkstra's Algorithm</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/DS/topological.c">Topological Sorting</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/DS/dfs.c">DFS</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/DS/bfs.c">BFS</a></li>
         </ul>
         `;
   }
@@ -234,11 +104,11 @@ function openModal(content) {
             <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/tree/main/S1/WEB/RegistrationForm">Registration Form</a></li>
             <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/tree/main/S1/WEB/factorial.php">Factorial</a></li>
             <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/tree/main/S1/WEB/signup">Login Form</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/tree/main/S1/WEB/JavaScriptFunctions">JavaScript Functions</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/tree/main/S1/WEB/ChangeBackgroundColor">JavaScript: Change Background Color</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/tree/main/S1/WEB/FormValidation">Form Validation</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/tree/main/S1/WEB/ChangeImageOnHover">JavaScript: Change Image on Hover</a></li>
-            <li><a href="https://github.com/CET-MCA-26/MCA-Laboratory/tree/main/S1/WEB/StudentRegistration">Student Registration</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/WEB/javascriptfns.html">JavaScript Functions</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/tree/main/S1/WEB/bgcolorchange">JavaScript: Change Background Color</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/WEB/formvalidation.html">Form Validation</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/blob/main/S1/WEB/changeimage_hover.html.html">JavaScript: Change Image on Hover</a></li>
+            <li><a href="https://github.com/cetmca26/MCA-Laboratory/tree/main/S1/WEB/Sample%20Qns/marks">Student Registration</a></li>
             </ul>
 
           `;
