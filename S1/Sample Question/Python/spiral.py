ca2d9317c346686eb6560a5372e1516bc350ada8
notmaal <aswin4147@gmail.com>
2025-01-18 23:37:52 +0530

    add(comments for spiral)

--- S1/Sample Question/Python/spiral.py
+++ S1/Sample Question/Python/spiral.py
@@ -1,33 +1,38 @@
 n = int(input("Enter the limit: "))
 
-a = [[0 for _ in range(n)] for _ in range(n)]
+array = [[0 for _ in range(n)] for _ in range(n)]   # stores the spiral in 2d array (first all are zero)
 
-num =1
+num =1   # for printing num from 1, 2, .. to n
 
-top, bottom, left, right = 0, n-1, 0, n-1
+top, bottom, left, right = 0, n-1, 0, n-1      # limits for each side
 
-while num<= n*n:
+while num<= n*n:       # eg: if limit is 5 the largest number would be 25 in a 2d matrix of size n x n
+    # adding the top row to array
     for i in range(left, right + 1):
-        a[top][i] = num
+        array[top][i] = num
         num+=1
     top+=1
 
+    # adding the right column to array
     for i in range(top, bottom + 1):
-        a[i][right] = num
+        array[i][right] = num
         num+=1
     right-=1
-    
+     
+    # adding the bottom row to array
     for i in range(right, left -1, -1):
-        a[bottom][i] = num
+        array[bottom][i] = num
         num+=1
     bottom-=1
 
+    # adding the left column to array
     for i in range(bottom, top -1, -1):
-        a[i][left] = num
+        array[i][left] = num
         num+=1
     left+=1
 
-for x in a:
-    for z in x:
-        print(z, end="\t")
+# printing the array contents
+for row in array:
+    for column in row:
+        print(column, end="\t")
     print("\n")
